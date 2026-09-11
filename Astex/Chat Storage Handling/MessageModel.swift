4245017e1fb65933a0e2517bbc84f09241e288d0
benyamin <60822041+wyznel@users.noreply.github.com>
2026-09-11 23:16:15 +0100

    chore: Remove legacy message store code.

--- Astex/Chat Storage Handling/MessageModel.swift
+++ Astex/Chat Storage Handling/MessageModel.swift
@@ -7,47 +7,11 @@ final class Message {
     var chat: Chat?
     var createdAt: Date
 
-    private var isUser: Bool = false
-    private var isThinking: Bool = false
-    private var isAToolCall: Bool = false
-
-    @Attribute(originalName: "type")
-    private var storedType: MessageType?
-
-    var type: MessageType {
-        get {
-            if let storedType {
-                return storedType
-            }
-
-            if isUser {
-                return .user
-            }
-
-            if isThinking {
-                return .llm(.thinking)
-            }
-
-            if isAToolCall {
-                return .llm(.tool)
-            }
-
-            return .llm(.response)
-        }
-        set {
-            storedType = newValue
-            isUser = newValue == .user
-            isThinking = newValue == .llm(.thinking)
-            isAToolCall = newValue == .llm(.tool)
-        }
-    }
+    var type: MessageType
 
     init(type: MessageType, response: String) {
         self.response = response
         self.createdAt = .now
-        self.storedType = type
-        self.isUser = type == .user
-        self.isThinking = type == .llm(.thinking)
-        self.isAToolCall = type == .llm(.tool)
+        self.type = type
     }
 }
